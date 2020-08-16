8f92a50ce34af4c707334fe284276552a029669b
“khuong-nguyen” <khuongnv@nexlesoft.com>
2020-08-16 17:36:09 +0700

    Structure table

--- database/migrations/2020_08_08_111704_create_employee_table.php
+++ database/migrations/2020_08_08_111704_create_employee_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomerTable extends Migration
+class CreateEmployeeTable extends Migration
 {
     /**
      * Run the migrations.
