040cb3a68c09cfc7568f59884e6afeb4f5644670
“khuong-nguyen” <khuongnv@nexlesoft.com>
2020-08-16 15:59:08 +0700

    Schedule Booking Container structure table.

---
+++ database/migrations/2020_08_08_111704_create_employee_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('employee_name', 200);
+            $table->string('employee_address', 300)->nullable();
+            $table->string('employee_code', 50)->unique();
+            $table->string('email', 100)->nullable();
+            $table->string('tel', 30)->nullable();
+            $table->string('tax_code', 50)->nullable();
+            $table->string('country_code', 30)->nullable();
+            $table->string('city', 30)->nullable();
+            $table->string('zip_code', 50)->nullable();
+            $table->string('department_code', 30)->comment('[DRIVER - HR - TRANSPORT - ACCOUNTING]');
+            $table->foreignId('created_by')->nullable();
+            $table->foreignId('updated_by')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
---
+++ database/migrations/2020_08_08_111704_create_fixed_asset_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fixed_asset', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fixed_asset_name', 200);
+            $table->string('manuafacture', 300)->nullable();
+            $table->string('fixed_asset_code', 50)->unique();
+            $table->string('desc', 255)->nullable();
+            $table->string('fixed_asset_type', 30)->comment('TRUCK - MOOC - RO-MOOC]');
+            $table->foreignId('created_by')->nullable();
+            $table->foreignId('updated_by')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
