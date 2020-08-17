96be383c408c841af476e5cbc94d1b828d8f8898
“khuong-nguyen” <khuongnv@nexlesoft.com>
2020-08-17 21:08:42 +0700

    fix conflict Employee & Customer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             CustomerRepository::class,
             function () {
-                return  new EloquentCustomerRepository(new Employee());
+                return  new EloquentCustomerRepository(new Customer());
             }
         );
         
