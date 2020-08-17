0d07e8e02700636434ca091115f3daeebaf0f265
“khuong-nguyen” <khuongnv@nexlesoft.com>
2020-08-17 07:01:11 +0700

    CreateAdvanceMoneyTable

--- database/migrations/2020_08_08_111704_create_advance_money_table.php
+++ database/migrations/2020_08_08_111704_create_advance_money_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFixedAssetTable extends Migration
+class CreateAdvanceMoneyTable extends Migration
 {
     /**
      * Run the migrations.
